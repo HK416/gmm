66e9f59232effae1e1f075600f420072d0422af2
HK416 <powerspirit127@gmail.com>
2024-04-17 14:17:24 +0900

    Add Float3 structure.

--- src/data/float3.rs
+++ src/data/float3.rs
@@ -1,24 +1,26 @@
 use core::fmt;
 
-/// A structure that stores two-dimensional vector data.
+/// A structure that stores three-dimensional vector data.
 #[repr(C)]
 #[derive(Clone, Copy, PartialEq)]
-pub struct Float2 {
+pub struct Float3 {
     pub x: f32,
     pub y: f32,
+    pub z: f32,
 }
 
-impl fmt::Debug for Float2 {
+impl fmt::Debug for Float3 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_tuple(stringify!(Float2))
+        f.debug_tuple(stringify!(Float3))
             .field(&self.x)
             .field(&self.y)
+            .field(&self.z)
             .finish()
     }
 }
 
-impl fmt::Display for Float2 {
+impl fmt::Display for Float3 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{{ {}, {} }}", &self.x, &self.y)
+        write!(f, "{{ {}, {}, {} }}", &self.x, &self.y, &self.z)
     }
 }
--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,1 +1,2 @@
 pub mod float2;
+pub mod float3;
