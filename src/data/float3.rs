2dbc9f1c90ab1c77850c6b368920146de637d8c9
HK416 <powerspirit127@gmail.com>
2024-04-17 14:41:09 +0900

    Add constructor and constants to Float3

--- src/data/float3.rs
+++ src/data/float3.rs
@@ -1,4 +1,5 @@
 use core::fmt;
+use core::f32;
 
 /// A structure that stores three-dimensional vector data.
 #[repr(C)]
@@ -7,6 +8,64 @@ pub struct Float3 {
     pub x: f32,
     pub y: f32,
     pub z: f32,
+}
+
+impl Float3 {
+    /// All elements are zero.
+    pub const ZERO: Self = Self::fill(0.0);
+
+    /// All elements are one.
+    pub const ONE: Self = Self::fill(1.0);
+
+    /// All elements are negative one.
+    pub const NEG_ONE: Self = Self::fill(-1.0);
+
+    /// positive unit vector on x-axis.
+    pub const X: Self = Self::new(1.0, 0.0, 0.0);
+
+    /// positive unit vector on y-axis.
+    pub const Y: Self = Self::new(0.0, 1.0, 0.0);
+
+    /// positive unit vector on z-axis.
+    pub const Z: Self = Self::new(0.0, 0.0, 1.0);
+
+    /// negative unit vector on x-axis.
+    pub const NEG_X: Self = Self::new(-1.0, 0.0, 0.0);
+
+    /// negative unit vector on y-axis.
+    pub const NEG_Y: Self = Self::new(0.0, -1.0, 0.0);
+
+    /// negative unit vector on z-axis.
+    pub const NEG_Z: Self = Self::new(0.0, 0.0, -1.0);
+
+    /// All elements are [`f32::MIN`].
+    pub const MIN: Self = Self::fill(f32::MIN);
+
+    /// All elements are [`f32::MAX`].
+    pub const MAX: Self = Self::fill(f32::MAX);
+
+    /// All elements are [`f32::NAN`].
+    pub const NAN: Self = Self::fill(f32::NAN);
+
+    /// All elements are [`f32::INFINITY`].
+    pub const INFINITY: Self = Self::fill(f32::INFINITY);
+
+    /// All elements are [`f32::NEG_INFINITY`].
+    pub const NEG_INFINITY: Self = Self::fill(f32::NEG_INFINITY);
+
+    /// Creates with given elements.
+    #[must_use]
+    #[inline(always)]
+    pub const fn new(x: f32, y: f32, z: f32) -> Self {
+        Self { x, y, z }
+    }
+
+    /// Fills all elements with the given values.
+    #[must_use]
+    #[inline(always)]
+    pub const fn fill(val: f32) -> Self {
+        Self { x: val, y: val, z: val }
+    }
 }
 
 impl fmt::Debug for Float3 {
