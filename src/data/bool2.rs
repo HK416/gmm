d2a2c98d3a2fe06398c85dcfda1aba36d597c77c
HK416 <powerspirit127@gmail.com>
2024-04-25 08:05:27 +0900

    Add constructors, constants, and functions to Boolean2

--- src/data/bool2.rs
+++ src/data/bool2.rs
@@ -7,6 +7,40 @@ pub struct Boolean2 {
     pub y: bool,
 }
 
+impl Boolean2 {
+    /// All elements are `true`.
+    pub const TRUE: Self = Self::fill(true);
+
+    /// All elements are `false`.
+    pub const FALSE: Self = Self::fill(false);
+
+    /// Creates with given elements.
+    #[must_use]
+    #[inline(always)]
+    pub const fn new(x: bool, y: bool) -> Self {
+        Self { x, y }
+    }
+
+    /// Fills all elements with the given values.
+    #[must_use]
+    #[inline(always)]
+    pub const fn fill(val: bool) -> Self {
+        Self { x: val, y: val }
+    }
+
+    /// Returns `true` if any of the elements are `true`.
+    #[inline]
+    pub const fn any(self) -> bool {
+        self.x | self.y
+    }
+
+    /// Returns `true` if all the elements are `true`.
+    #[inline]
+    pub const fn all(self) -> bool {
+        self.x & self.y
+    }
+}
+
 impl fmt::Debug for Boolean2 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_tuple(stringify!(Boolean2))
