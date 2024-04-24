22fd31f218b5bb22c5f5904e207e7510e645d278
HK416 <powerspirit127@gmail.com>
2024-04-25 08:08:44 +0900

    Add constructors, constants, and functions to Boolean3

--- src/data/bool3.rs
+++ src/data/bool3.rs
@@ -8,6 +8,40 @@ pub struct Boolean3 {
     pub z: bool,
 }
 
+impl Boolean3 {
+    /// All elements are `true`.
+    pub const TRUE: Self = Self::fill(true);
+
+    /// All elements are `false`.
+    pub const FALSE: Self = Self::fill(false);
+
+    /// Creates with given elements.
+    #[must_use]
+    #[inline(always)]
+    pub const fn new(x: bool, y: bool, z: bool) -> Self {
+        Self { x, y, z }
+    }
+
+    /// Fills all elements with the given values.
+    #[must_use]
+    #[inline(always)]
+    pub const fn fill(val: bool) -> Self {
+        Self { x: val, y: val, z: val }
+    }
+
+    /// Returns `true` if any of the elements are `true`.
+    #[inline]
+    pub const fn any(self) -> bool {
+        self.x | self.y | self.z
+    }
+
+    /// Returns `true` if all elements are `true`.
+    #[inline]
+    pub const fn all(self) -> bool {
+        self.x & self.y & self.z
+    }
+}
+
 impl fmt::Debug for Boolean3 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_tuple(stringify!(Boolean3))
