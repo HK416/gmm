3fa1c77ac7070786438dddb093c1b1e424ef7bca
HK416 <powerspirit127@gmail.com>
2024-04-25 08:17:53 +0900

    Add some trait implementations to Boolean2

--- src/data/bool2.rs
+++ src/data/bool2.rs
@@ -1,4 +1,8 @@
 use core::fmt;
+use core::ops;
+
+use super::bool3::Boolean3;
+use super::bool4::Boolean4;
 
 #[repr(C)]
 #[derive(Clone, Copy, PartialEq, Eq)]
@@ -38,6 +42,64 @@ impl Boolean2 {
     #[inline]
     pub const fn all(self) -> bool {
         self.x & self.y
+    }
+}
+
+impl Default for Boolean2 {
+    #[inline(always)]
+    fn default() -> Self {
+        Self::FALSE
+    }
+}
+
+impl From<Boolean3> for Boolean2 {
+    #[inline]
+    fn from(value: Boolean3) -> Self {
+        Boolean2 { x: value.x, y: value.y }
+    }
+}
+
+impl From<Boolean4> for Boolean2 {
+    #[inline]
+    fn from(value: Boolean4) -> Self {
+        Boolean2 { x: value.x, y: value.y }
+    }
+}
+
+impl AsRef<[bool; 2]> for Boolean2 {
+    #[inline]
+    fn as_ref(&self) -> &[bool; 2] {
+        unsafe { &*(self as *const Boolean2 as *const [bool; 2]) }
+    }
+}
+
+impl AsMut<[bool; 2]> for Boolean2 {
+    #[inline]
+    fn as_mut(&mut self) -> &mut [bool; 2] {
+        unsafe { &mut *(self as *mut Boolean2 as *mut [bool; 2]) }
+    }
+}
+
+impl ops::Index<usize> for Boolean2 {
+    type Output = bool;
+    #[inline]
+    fn index(&self, index: usize) -> &Self::Output {
+        match index {
+            0 => &self.x,
+            1 => &self.y,
+            _ => panic!("index out of range!"),
+        }
+    }
+}
+
+impl ops::IndexMut<usize> for Boolean2 {
+    #[inline]
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        match index {
+            0 => &mut self.x,
+            1 => &mut self.y,
+            _ => panic!("index out of range!"),
+        }
     }
 }
 
