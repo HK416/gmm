c5604471f89760f6b69ad329880ad57ccee6bb89
HK416 <powerspirit127@gmail.com>
2024-05-04 23:23:49 +0900

    Addconstructor and constants to UInteger3

--- src/data/uint3.rs
+++ src/data/uint3.rs
@@ -9,6 +9,41 @@ pub struct UInteger3 {
     pub z: u32,
 }
 
+impl UInteger3 {
+    /// All elements are zero.
+    pub const ZERO: Self = Self::fill(0);
+
+    /// All elements are one.
+    pub const ONE: Self = Self::fill(1);
+
+    /// positive unit vector on x-axis.
+    pub const X: Self = Self::new(1, 0, 0);
+    
+    /// positive unit vector on y-axis.
+    pub const Y: Self = Self::new(0, 1, 0);
+
+    /// positive unit vector on z-axis.
+    pub const Z: Self = Self::new(0, 0, 1);
+
+    /// All elements are [`u32::MIN`].
+    pub const MIN: Self = Self::fill(u32::MIN);
+
+    /// All elements are [`u32::MAX`].
+    pub const MAX: Self = Self::fill(u32::MAX);
+
+    #[must_use]
+    #[inline(always)]
+    pub const fn new(x: u32, y: u32, z: u32) -> Self {
+        Self { x, y, z }
+    }
+
+    #[must_use]
+    #[inline(always)]
+    pub const fn fill(val: u32) -> Self {
+        Self { x: val, y: val, z: val }
+    }
+}
+
 impl fmt::Debug for UInteger3 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_tuple(stringify!(UInteger3))
