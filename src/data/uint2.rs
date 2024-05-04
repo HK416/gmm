69a97255fc3012f0eee8f5b0b6c98cfa49757999
HK416 <powerspirit127@gmail.com>
2024-05-04 23:21:19 +0900

    Addconstructor and constants to UInteger2

--- src/data/uint2.rs
+++ src/data/uint2.rs
@@ -8,6 +8,40 @@ pub struct UInteger2 {
     pub y: u32,
 }
 
+impl UInteger2 {
+    /// All elements are zero.
+    pub const ZERO: Self = Self::fill(0);
+
+    /// All elements are one.
+    pub const ONE: Self = Self::fill(1);
+
+    /// positive unit vector on x-axis.
+    pub const X: Self = Self::new(1, 0);
+    
+    /// positive unit vector on y-axis.
+    pub const Y: Self = Self::new(0, 1);
+
+    /// All elements are [`u32::MIN`].
+    pub const MIN: Self = Self::fill(u32::MIN);
+
+    /// All elements are [`u32::MAX`].
+    pub const MAX: Self = Self::fill(u32::MAX);
+
+    /// Creates with given elements.
+    #[must_use]
+    #[inline(always)]
+    pub const fn new(x: u32, y: u32) -> Self {
+        Self { x, y }
+    }
+
+    /// Fills all elements with the given values.
+    #[must_use]
+    #[inline(always)]
+    pub const fn fill(val: u32) -> Self {
+        Self { x: val, y: val }
+    }
+}
+
 impl fmt::Debug for UInteger2 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_tuple(stringify!(UInteger2))
