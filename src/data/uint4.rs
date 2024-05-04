026849dfccc51aa1edd0d173985dba03be97bacb
HK416 <powerspirit127@gmail.com>
2024-05-04 23:48:32 +0900

    Add some trait implementations to UInteger4

--- src/data/uint4.rs
+++ src/data/uint4.rs
@@ -1,4 +1,9 @@
 use core::fmt;
+use core::ops;
+
+use super::bool4::Boolean4;
+use super::uint2::UInteger2;
+use super::uint3::UInteger3;
 
 #[repr(C)]
 #[derive(Clone, Copy, PartialEq, Eq)]
@@ -44,6 +49,80 @@ impl UInteger4 {
     #[inline(always)]
     pub const fn fill(val: u32) -> Self {
         Self { x: val, y: val, z: val, w: val }
+    }
+}
+
+impl Default for UInteger4 {
+    #[inline(always)]
+    fn default() -> Self {
+        Self::ZERO
+    }
+}
+
+impl Into<Boolean4> for UInteger4 {
+    #[inline]
+    fn into(self) -> Boolean4 {
+        Boolean4 { 
+            x: self.x == 0xFFFFFFFF, 
+            y: self.y == 0xFFFFFFFF, 
+            z: self.z == 0xFFFFFFFF, 
+            w: self.w == 0xFFFFFFFF 
+        }
+    }
+}
+
+impl From<UInteger2> for UInteger4 {
+    #[inline]
+    fn from(value: UInteger2) -> Self {
+        UInteger4 { x: value.x, y: value.y, z: 0, w: 0 }
+    }
+}
+
+impl From<UInteger3> for UInteger4 {
+    #[inline]
+    fn from(value: UInteger3) -> Self {
+        UInteger4 { x: value.x, y: value.y, z: value.z, w: 0 }
+    }
+}
+
+impl AsRef<[u32; 4]> for UInteger4 {
+    #[inline]
+    fn as_ref(&self) -> &[u32; 4] {
+        unsafe { &*(self as *const UInteger4 as *const [u32; 4]) }
+    }
+}
+
+impl AsMut<[u32; 4]> for UInteger4 {
+    #[inline]
+    fn as_mut(&mut self) -> &mut [u32; 4] {
+        unsafe { &mut *(self as *mut UInteger4 as *mut [u32; 4]) }
+    }
+}
+
+impl ops::Index<usize> for UInteger4 {
+    type Output = u32;
+    #[inline]
+    fn index(&self, index: usize) -> &Self::Output {
+        match index {
+            0 => &self.x,
+            1 => &self.y,
+            2 => &self.z,
+            3 => &self.w,
+            _ => panic!("index out of range!"),
+        }
+    }
+}
+
+impl ops::IndexMut<usize> for UInteger4 {
+    #[inline]
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        match index {
+            0 => &mut self.x,
+            1 => &mut self.y,
+            2 => &mut self.z,
+            3 => &mut self.w,
+            _ => panic!("index out of range!"),
+        }
     }
 }
 
